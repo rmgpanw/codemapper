e45ff583bc49f09894ba5b9077c48309aff8cc3c
rmgpanw <rmgpanw@ucl.ac.uk>
2022-03-23 10:26:58 +0000

    tweak dplyr::distinct() in map_codes_ukb_clinical_events

--- R/phecodes.R
+++ R/phecodes.R
@@ -194,8 +194,7 @@ map_codes_ukb_clinical_events <- function(clinical_events,
     dplyr::select(tidyselect::all_of(ukbwranglr:::CLINICAL_EVENTS_COLHEADERS)) %>%
 
     # remove duplicated rows
-    dplyr::distinct(tidyselect::all_of(ukbwranglr:::CLINICAL_EVENTS_COLHEADERS),
-                    .keep_all = TRUE)
+    dplyr::distinct()
 
   # return result
   return(result)
