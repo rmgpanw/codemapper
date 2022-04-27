30ca0d349e483e2ee485fb1edb3f3cc7ccedf50d
rmgpanw <rmgpanw@ucl.ac.uk>
2022-04-27 09:13:59 +0100

    remove non-R package files/directories

--- _gitlab-ci.dependencies.R
+++
@@ -1,9 +0,0 @@
-# other packages required for gitlab CI
-library(DT)
-library(covr)
-library(devtools)
-library(remotes)
-install.packages("textshaping", type = "binary")
-devtools::install_version("textshaping", version = "0.3.5")
-library(textshaping)
-library(pkgdown)
