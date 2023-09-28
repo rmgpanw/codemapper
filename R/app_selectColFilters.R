191b8da575505e580cd93b5bf1362fd8eb81ae79
rmgpanw <rmgpanw@ucl.ac.uk>
2023-09-28 09:06:03 +0100

    update app_selectColFilters.R: fix 'cancel' button when all choices are unselected, add option to show/hide selected col_filters, remove lobstr::tree()

--- R/app_selectColFilters.R
+++ R/app_selectColFilters.R
@@ -1,7 +1,7 @@
-selectColFiltersInput <- function(id) {
+selectColFiltersInput <- function(id, display_filters = FALSE) {
   ns <- NS(id)
 
-  # create UI programatically
+  # create select col_filters UI elements programatically
   ui_inputs <- get_col_filters() %>%
     purrr::imap(\(x, idx) {
       tab <- idx
@@ -26,7 +26,8 @@ selectColFiltersInput <- function(id) {
       )
     })
 
-  tagList(
+  # All UI elements
+  ui_list <- list(
     actionButton(
       ns("restore_defaults"),
       "Restore defaults",
@@ -40,15 +41,28 @@ selectColFiltersInput <- function(id) {
     fluidRow(
       column(6, actionButton(ns("update_selections"), "Update selections", class = "btn-lg btn-danger")),
       column(6, actionButton(ns("cancel"), "Cancel", class = "btn-lg"))
-    ),
+    ))
+
+  if (display_filters) {
+
+  display_filters_ui <- list(
     fluidRow(
       column(6, h1("Selected"),
              verbatimTextOutput(ns("selected"))),
-      column(6, h1("All filters"),
-             verbatimTextOutput(ns("all_filters")))
+      column(6, h1("Confirmed selected"),
+             verbatimTextOutput(ns("confirmed_selected")))
     ),
-    h1("Confirmed selected"),
-    verbatimTextOutput(ns("confirmed_selected"))
+    h1("All filters"),
+    verbatimTextOutput(ns("all_filters"))
+  )
+
+  ui_list <- c(ui_list,
+               display_filters_ui)
+  }
+
+  # UI
+  tagList(
+    ui_list
   )
 }
 
@@ -65,11 +79,6 @@ selectColFiltersServer <- function(id) {
             purrr::imap(\(x, idx) input[[paste(tab, idx, sep = ".")]])
         })
     })
-
-    output$selected <- renderPrint(lobstr::tree(selected_filters()))
-
-    output$all_filters <-
-      renderPrint(lobstr::tree(get_col_filters(FALSE)))
 
     # restore default selections, on request
     observeEvent(input$restore_defaults, {
@@ -94,7 +103,13 @@ selectColFiltersServer <- function(id) {
       selected_filters()
     })
 
-    output$confirmed_selected <- renderPrint(lobstr::tree(confirmed_selected_filters()))
+    # display select/confirmed col_filters (for debugging)
+    output$selected <- renderPrint(selected_filters())
+
+    output$all_filters <-
+      renderPrint(get_col_filters(FALSE))
+
+    output$confirmed_selected <- renderPrint(confirmed_selected_filters())
 
     # cancel selected choices (restore to most recently confirmed choices)
     observeEvent(input$cancel, {
@@ -104,11 +119,16 @@ selectColFiltersServer <- function(id) {
 
           get_col_filters(defaults_only = FALSE,
                           selected_table = idx) %>%
-            purrr::imap(\(x, idx)
-                        updateCheckboxGroupInput(
-                          inputId = paste(tab, idx, sep = "."),
-                          selected = confirmed_selected_filters()[[tab]][[idx]]
-                        )) %>%
+            purrr::imap(\(x, idx) {
+              selected <- confirmed_selected_filters()[[tab]][[idx]]
+
+              if (is.null(selected)) {
+                selected <- character(0)
+              }
+
+              updateCheckboxGroupInput(inputId = paste(tab, idx, sep = "."),
+                                       selected = selected)
+            }) %>%
             purrr::set_names(NULL)
         })
     })
@@ -120,7 +140,7 @@ selectColFiltersServer <- function(id) {
 
 # for testing
 selectColFiltersApp <- function() {
-  ui <- fluidPage(selectColFiltersInput("col_filters"))
+  ui <- fluidPage(selectColFiltersInput("col_filters", display_filters = TRUE))
 
   server <- function(input, output, session) {
     selectColFiltersServer("col_filters")
