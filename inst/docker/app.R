e93de776c1a99c8578cb436376fd140e54fd7a32
rmgpanw <rmgpanw@ucl.ac.uk>
2023-11-14 11:54:54 +0000

    add dockerfile and github actions workflow for building codemapepr app image

---
+++ inst/docker/app.R
@@ -0,0 +1,14 @@
+# By default, use db containing only publicly available resources
+ALL_LKPS_MAPS_DB <- "./all_lkps_maps.db"
+
+# User can supply their own db by mounting a folder called `all_lkps_maps`
+# containing `all_lkps_maps.db`, to the container root directory
+if (dir.exists("/all_lkps_maps")) {
+  USER_SUPPLIED_DB_PATH <- "/all_lkps_maps/all_lkps_maps.db"
+  if (file.exists(USER_SUPPLIED_DB_PATH)) {
+    ALL_LKPS_MAPS_DB <- USER_SUPPLIED_DB_PATH
+  }
+}
+
+# Run app
+codemapper::RunCodelistBuilder(all_lkps_maps = ALL_LKPS_MAPS_DB)
