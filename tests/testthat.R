16b7d39db64c5c69db42ded792bdee15e3530a60
rmgpanw <rmgpanw@ucl.ac.uk>
2025-11-08 09:53:36 +0000

    add gh-pages pkgdown site

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(codemapper)
-
-test_check("codemapper")
--- tests/testthat/apps/RunCodelistBuilder/app.R
+++
@@ -1,8 +0,0 @@
-library(codemapper)
-
-all_lkps_maps_dummy <- build_all_lkps_maps_dummy()
-
-RunCodelistBuilder(
-  all_lkps_maps = all_lkps_maps_dummy,
-  options = NULL
-)
